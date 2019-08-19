b34ad82741ea16bf1120a2a4d903037e7b803f75
kelvinsun <kelvinsun@tencent.com>
2019-08-19 16:43:09 +0800

    feat: 准备开始解析

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,28 @@
-var md = new MobileDetect(window.navigator.userAgent);
 var $noticeDesktop = document.getElementById('notice-desktop');
 var $containerMain = document.getElementById('container-main');
 var $btnDesktopShow = document.getElementById('btn-desktop-show');
+
+var md;
 
 var showMain = function () {
     $noticeDesktop.classList.remove('active');
     $containerMain.classList.add('active');
 };
 
-if (null === md.mobile()) {
-    // 不处理
-} else {
-    showMain();
-}
+var init = function () {
+    md = new MobileDetect(window.navigator.userAgent);
+
+    if (null === md.mobile()) {
+        // 不处理
+    } else {
+        showMain();
+    }
+};
 
 $btnDesktopShow.addEventListener('click', function () {
     showMain();
 });
+
+init();
 
 
